1a86b6ce705d6e6ad441c18a178f4a76c20f8130
Sourav <sourav@Souravs-MacBook-Pro.local>
2018-08-23 18:17:58 +0530

    seperate python folder created

---
+++ Python/Binary_Search.py
@@ -0,0 +1,30 @@
+import math
+
+# This method is Iterative way to find a element is Sorted array
+# Algo : Binary Search
+
+def bin_search(arr,l,r,x):
+    
+   while(l <= r):
+
+    mid = math.floor(l + (r - l)/2);
+    if (arr[mid] == x):
+        return mid
+    elif (arr[mid] > x):
+        r = mid-1
+    else:
+         l = mid+1
+
+   return -1
+
+arr = [2, 3, 4, 10, 40, 50, 53, 56, 69, 70]
+x = 50
+ 
+########### Function call ############
+
+result = bin_search(arr, 0, len(arr)-1, x)
+ 
+if result != -1:
+    print ("Element is present at index %d" %result)
+else:
+    print ("Element is not present in array")
