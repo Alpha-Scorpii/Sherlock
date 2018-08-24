d08f8987f54874c3f0809527f23b383d007aade1
Sourav <sourav@Souravs-MacBook-Pro.local>
2018-08-24 17:30:48 +0530

    Mathematics section of Hacker-Rank First problem Solution

---
+++ HackerRank/Mathematics/Maximum_draws.py
@@ -0,0 +1,44 @@
+#
+# Complete the maximumDraws function below.
+#
+
+def maximumDraws(n):
+    #
+    # Write your code here.
+    #
+        if(n==0):
+            return 0
+        else:
+            return n+1
+   
+#
+# This Function code is part of HackerRank and to be used in HackerRank console.
+#
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     t = int(input())
+
+#     for t_itr in range(t):
+#         n = int(input())
+
+#         result = maximumDraws(n)
+
+#         fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+############# Driver Function ############
+
+t = int(input("Enter Number of testcases"))
+type(t)
+
+while(t!=0):
+
+    n = int(input("Enter number of pair of socks"))
+    type(n)
+
+    print(maximumDraws(n))
+    t = t-1    
